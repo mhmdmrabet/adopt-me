e077488105154ba2264d354010450bc633c26a52
Mohamed <mhmdmrabet@yahoo.fr>
2021-06-05 14:57:54 +0200

    Class Components

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,45 @@
-const Details = () => {
-  return <h2> Hi EveryBody !</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
 
-export default Details;
+  async componentDidMount() {
+    const response = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await response.json();
+
+    // === NEED TO CHECK
+    // this.setState({ loading: false, ...json.pets[0] });
+
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    const { animal, city, state, breed, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {city}, {state}
+        </h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
